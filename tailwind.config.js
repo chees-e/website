231f6cb46747e5d8380d76f193f864cb25d6a6d1
Shawn <shawnlu4@gmail.com>
2023-08-07 00:16:16 -0700

    moved some photo layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     screens: {
       md: '768px',
       lg: '1024px',
-      xl: '1440px',
+      xl: '1600px',
     },
     fontFamily: {
       'serif' : ['New Century Schoolbook', 'TeX Gyre Schola',],
